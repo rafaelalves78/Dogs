163cfd79225e3be5d728c61372806405d06a4b97
Rafael Fernandes <rafaelalves78@gmail.com>
2025-03-13 14:36:33 -0300

    Estrutura inicial

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <h2>footer</h2>
+    </div>
+  );
+};
+
+export default Footer;
