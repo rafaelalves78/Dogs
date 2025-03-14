198136d9d7a4d3ecf1be2954c9f6c5a17ea214dd
Rafael Fernandes <rafaelalves78@gmail.com>
2025-03-13 23:23:41 -0300

    Criando o useForm

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input id={name} type={type} name={name} className={styles.input}></input>
-      <p className={styles.error}>Error</p>
+      <input
+        id={name}
+        type={type}
+        name={name}
+        className={styles.input}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      ></input>
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
---
+++ src/Hooks/useForm.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const types = {
+  email: {
+    regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    message: 'Preencha um email válido',
+  },
+};
+
+const useForm = (type) => {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
+
+  const validate = (value) => {
+    if (type === false) return true;
+    if (value.length === 0) {
+      setError('Preencha um valor');
+      return false;
+    } else if (types[type] && !types[type].regex.test(value)) {
+      setError(types[type].message);
+      return false;
+    } else {
+      setError(null);
+      return true;
+    }
+  };
+
+  const onChange = ({ target }) => {
+    setValue(target.value);
+    if (error) validate(target.value); // Validação em tempo real
+  };
+
+  return {
+    value,
+    setValue,
+    onChange,
+    error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
+  };
+};
+
+export default useForm;
